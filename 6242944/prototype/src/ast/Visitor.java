6f48421fcb544f70340c7f448a25f3a2bfa8a8b0
jandemooij <jandemooij@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-01-06 22:42:28 +0000

    Implement relational op

--- 6242944/prototype/src/ast/Visitor.java
+++ 6242944/prototype/src/ast/Visitor.java
@@ -7,6 +7,9 @@ public abstract class Visitor<T> {
 	protected abstract T visit(AddExpression e);
 	protected abstract T visit(SubExpression e);
 	
+	protected abstract T visit(LtExpression e);
+
+	
 	protected abstract T visit(IntegerLiteral e);
 
 	protected abstract T visit(ProcedureCall procedureCall);
