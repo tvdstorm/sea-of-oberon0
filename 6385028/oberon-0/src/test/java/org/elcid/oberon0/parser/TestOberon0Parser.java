8d85b1923c73b1f66640333a17ee57c5a1b89ff1
pbrandwijk <pbrandwijk@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-01-08 15:53:35 +0000

    Set testing file to quicksort.oberon0

--- 6385028/oberon-0/src/test/java/org/elcid/oberon0/parser/TestOberon0Parser.java
+++ 6385028/oberon-0/src/test/java/org/elcid/oberon0/parser/TestOberon0Parser.java
@@ -37,7 +37,7 @@ public class TestOberon0Parser {
 	@Test
 	public void testOberon0Parser() throws Exception {
 		// Create an input character stream from smoke test file
-		ANTLRInputStream input = new ANTLRInputStream(ClassLoader.getSystemResourceAsStream("org/elcid/oberon0/parser/sample.oberon0"));
+		ANTLRInputStream input = new ANTLRInputStream(ClassLoader.getSystemResourceAsStream("org/elcid/oberon0/parser/quicksort.oberon0"));
 		// Create an Oberon0Lexer that feeds from that stream
 		Oberon0Lexer lexer = new Oberon0Lexer(input);
 		// Create a stream of tokens fed by the lexer
