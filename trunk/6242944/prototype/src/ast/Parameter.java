1e3152698fac28bcf66880b9d24a866be6fbf775
jandemooij <jandemooij@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-01-10 14:40:48 +0000

    ref args

--- trunk/6242944/prototype/src/ast/Parameter.java
+++ trunk/6242944/prototype/src/ast/Parameter.java
@@ -3,10 +3,12 @@ package ast;
 public class Parameter extends Node {
 	private String _name;
 	private Type _type;
+	private boolean _byRef;
 	
-	public Parameter(String name, Type type) {
+	public Parameter(String name, Type type, boolean byRef) {
 		this._name = name;
 		this._type = type;
+		this._byRef = byRef;
 	}
 	
 	@Override
@@ -15,4 +17,11 @@ public class Parameter extends Node {
 		return null;
 	}
 
+	public String getName() {
+		return _name;
+	}
+	
+	public boolean isByRef() {
+		return _byRef;
+	}
 }
