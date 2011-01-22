41dc57fade183d557d41721be6444b594b52d2cc
pbrandwijk <pbrandwijk@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-01-22 18:59:55 +0000

    Added Expression node as base class for expressions

---
+++ trunk/6385028/oberon-0/src/main/java/org/elcid/oberon0/ast/ExpressionNode.java
@@ -0,0 +1,10 @@
+package org.elcid.oberon0.ast;
+
+/**
+ * Base class for Oberon-0 expressions. Not meant to be instantiated directly, so it's abstract
+ *
+ * @author Pieter Brandwijk
+ */
+public abstract class ExpressionNode extends Node {
+
+}
