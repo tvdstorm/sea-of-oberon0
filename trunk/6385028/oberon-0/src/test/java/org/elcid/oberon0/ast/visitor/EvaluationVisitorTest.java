8f5a089e8623bfb71a781a6f2bbdd579ba364d08
pbrandwijk <pbrandwijk@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-01-22 19:15:15 +0000

    Updated test method for binary expression

--- trunk/6385028/oberon-0/src/test/java/org/elcid/oberon0/ast/visitor/EvaluationVisitorTest.java
+++ trunk/6385028/oberon-0/src/test/java/org/elcid/oberon0/ast/visitor/EvaluationVisitorTest.java
@@ -1,7 +1,7 @@
 package org.elcid.oberon0.ast.visitor;
 
 import org.elcid.oberon0.ast.EqualsNode;
-import org.elcid.oberon0.ast.IntegerNode;
+import org.elcid.oberon0.ast.IntegerExpNode;
 import org.elcid.oberon0.ast.BinaryExpNode;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -49,11 +49,19 @@ public class EvaluationVisitorTest {
 
 	@Test
 	public void testVisitBinaryExpNode() {
-		BinaryExpNode trueBinExp = new BinaryExpNode(new IntegerNode(2), new IntegerNode(2), new EqualsNode());
-		assertTrue((Boolean) trueBinExp.accept(new EvaluationVisitor()));
+		EvaluationVisitor visitor = new EvaluationVisitor();
 
-		BinaryExpNode falseBinExp = new BinaryExpNode(new IntegerNode(3), new IntegerNode(2), new EqualsNode());
-		assertFalse((Boolean) falseBinExp.accept(new EvaluationVisitor()));
+		BinaryExpNode trueBinExp = new BinaryExpNode(new IntegerExpNode(2), new IntegerExpNode(2), new EqualsNode());
+		assertTrue((Boolean) trueBinExp.accept(visitor));
+
+		BinaryExpNode falseBinExp = new BinaryExpNode(new IntegerExpNode(3), new IntegerExpNode(2), new EqualsNode());
+		assertFalse((Boolean) falseBinExp.accept(visitor));
+
+		BinaryExpNode falseLogicalBinExp = new BinaryExpNode(
+				new BinaryExpNode(new IntegerExpNode(3), new IntegerExpNode(3), new EqualsNode() ), 
+				new BinaryExpNode(new IntegerExpNode(2), new IntegerExpNode(2), new EqualsNode() ),
+				new EqualsNode());
+		assertTrue((Boolean) falseLogicalBinExp.accept(visitor));
 	}
 
 }
