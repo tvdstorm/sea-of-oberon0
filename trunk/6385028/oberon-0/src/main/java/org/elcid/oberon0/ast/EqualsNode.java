c8c9c783f6a0296572dec22d553e57503fca73fa
pbrandwijk <pbrandwijk@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-01-22 18:49:31 +0000

    Added node classes

---
+++ trunk/6385028/oberon-0/src/main/java/org/elcid/oberon0/ast/EqualsNode.java
@@ -0,0 +1,31 @@
+package org.elcid.oberon0.ast;
+
+import java.util.List;
+import org.elcid.oberon0.ast.visitor.NodeVisitor;
+
+/**
+ *
+ * @author Pieter Brandwijk
+ */
+public class EqualsNode extends Node {
+
+	public EqualsNode() {
+		
+	}
+
+	@Override
+	public Object accept(NodeVisitor visitor) {
+		throw new UnsupportedOperationException("Not supported yet.");
+	}
+
+	@Override
+	public List<Node> childNodes() {
+		return EMPTY_LIST;
+	}
+
+	@Override
+	public NodeType getNodeType() {
+		return NodeType.EQUALSNODE;
+	}
+
+}
