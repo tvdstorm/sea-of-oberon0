488bf722f3c8ac96c14b36e10feecd8c0e27f557
pbrandwijk <pbrandwijk@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-01-22 15:13:29 +0000

    Renamed and expanded IdentifierNode

---
+++ 6385028/oberon-0/src/main/java/org/elcid/oberon0/ast/IdentifierNode.java
@@ -0,0 +1,38 @@
+package org.elcid.oberon0.ast;
+
+import java.util.List;
+import org.elcid.oberon0.ast.visitor.NodeVisitor;
+
+/**
+ * Representation of an identifier in the Oberon-0 language.
+ *
+ * @author Pieter Brandwijk
+ */
+public class IdentifierNode extends Node {
+
+	/** The identifier's value can only be set once, when the object is constructed */
+	private final String value;
+
+	public IdentifierNode(String value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	@Override
+	public List<Node> childNodes() {
+		return EMPTY_LIST;
+	}
+
+	@Override
+	public Object accept(NodeVisitor iVisitor) {
+		return iVisitor.visitIdentifierNode(this);
+	}
+
+	@Override
+	public NodeType getNodeType() {
+		return NodeType.IDENTIFIERNODE;
+	}
+}
