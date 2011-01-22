5592fde16815830b468491e9988485f2fcc347c8
pbrandwijk <pbrandwijk@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-01-22 13:31:24 +0000

    Added base class and enumeration for AST

---
+++ trunk/6385028/oberon-0/src/main/java/org/elcid/oberon0/ast/NodeType.java
@@ -0,0 +1,13 @@
+package org.elcid.oberon0.ast;
+
+/**
+ * Enummeration of all possible node types in the oberon-0 AST.
+ *
+ * @author Pieter Brandwijk
+ */
+public enum NodeType {
+	INTEGERNODE,
+	IDENTIFIERNODE,
+	ARRAYNODE,
+	MODULENODE;
+}
