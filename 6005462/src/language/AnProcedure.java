af8ca6a02c079c91c0b3a3fd3c7196d9c2290c33
janssen.sjuul@gmail.com <janssen.sjuul@gmail.com@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-01-23 21:02:44 +0000

    oeps vergeten mee te committen

---
+++ 6005462/src/language/AnProcedure.java
@@ -0,0 +1,15 @@
+package language;
+
+public class AnProcedure implements IAstNode {
+
+	public AnProcedure(){
+		
+	}
+	
+	@Override
+	public IAstNode eval() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
