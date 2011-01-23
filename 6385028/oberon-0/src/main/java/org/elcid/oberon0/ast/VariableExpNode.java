dc3594122f25e881bc82e8a8aaac3b1c41e07904
pbrandwijk <pbrandwijk@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-01-23 11:03:53 +0000

    Added VariableNode

---
+++ 6385028/oberon-0/src/main/java/org/elcid/oberon0/ast/VariableExpNode.java
@@ -0,0 +1,38 @@
+package org.elcid.oberon0.ast;
+
+import java.util.List;
+import org.elcid.oberon0.ast.util.NodeType;
+import org.elcid.oberon0.ast.visitor.NodeVisitor;
+
+/**
+ * Represents a variable expression in the Oberon-0 language.
+ *
+ * @author Pieter Brandwijk
+ */
+public class VariableExpNode extends ExpressionNode {
+
+	private IdentifierNode variableName;
+
+	public VariableExpNode(IdentifierNode variableName) {
+		this.variableName = variableName;
+	}
+
+	public IdentifierNode getValue() {
+		return variableName;
+	}
+
+	@Override
+	public List<Node> childNodes() {
+		return EMPTY_LIST;
+	}
+
+	@Override
+	public Object accept(NodeVisitor visitor) {
+		return visitor.visitVariableExpNode(this);
+	}
+
+	@Override
+	public NodeType getNodeType() {
+		return NodeType.VARIABLEEXPNODE;
+	}
+}
