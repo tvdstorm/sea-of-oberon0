684c43237f8356e46c6be80f3e4014c07a89213c
pbrandwijk <pbrandwijk@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-01-23 11:07:16 +0000

    Added base class for operators

---
+++ trunk/6385028/oberon-0/src/main/java/org/elcid/oberon0/ast/OperatorNode.java
@@ -0,0 +1,11 @@
+package org.elcid.oberon0.ast;
+
+/**
+ * Base class for operators of the Oberon-0 language. Not meant to be
+ * instantiated directly, so it's abstract.
+ *
+ * @author Pieter Brandwijk
+ */
+public abstract class OperatorNode extends Node {
+
+}
