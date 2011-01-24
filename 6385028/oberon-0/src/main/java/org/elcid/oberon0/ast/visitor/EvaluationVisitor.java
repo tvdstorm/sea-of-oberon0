b7077c7c84260d776b2d78aff6472cfd82cad87d
pbrandwijk <pbrandwijk@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-01-24 19:13:24 +0000

    Changed operators to expressions

--- 6385028/oberon-0/src/main/java/org/elcid/oberon0/ast/visitor/EvaluationVisitor.java
+++ 6385028/oberon-0/src/main/java/org/elcid/oberon0/ast/visitor/EvaluationVisitor.java
@@ -1,6 +1,7 @@
 package org.elcid.oberon0.ast.visitor;
 
 import org.elcid.oberon0.ast.*;
+import org.elcid.oberon0.ast.util.NodeType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,12 +30,40 @@ public class EvaluationVisitor implements NodeVisitor {
 	}
 
 	@Override
+	public Object visitVariableExpNode(VariableExpNode node) {
+		return node.getValue().accept(this);
+	}
+
+	@Override
 	public Object visitEqualsExpNode(EqualsExpNode node) {
 		return node.getLeftExp().accept(this) == node.getRightExp().accept(this);
 	}
 
 	@Override
-	public Object visitVariableExpNode(VariableExpNode node) {
-		return node.getValue().accept(this);
+	public Object visitGreaterExpNode(GreaterExpNode node) {
+		assert node.getLeftExp().getNodeType() == NodeType.INTEGERNODE: "Left node is not an integer node";
+		assert node.getRightExp().getNodeType() == NodeType.INTEGERNODE: "Right node is not an integer node";
+		return (Integer) node.getLeftExp().accept(this) > (Integer) node.getRightExp().accept(this);
+	}
+
+	@Override
+	public Object visitGreaterOrEqualsExpNode(GreaterOrEqualsExpNode node) {
+		assert node.getLeftExp().getNodeType() == NodeType.INTEGERNODE: "Left node is not an integer node";
+		assert node.getRightExp().getNodeType() == NodeType.INTEGERNODE: "Right node is not an integer node";
+		return (Integer) node.getLeftExp().accept(this) >= (Integer) node.getRightExp().accept(this);
+	}
+
+	@Override
+	public Object visitLesserExpNode(LesserExpNode node) {
+		assert node.getLeftExp().getNodeType() == NodeType.INTEGERNODE: "Left node is not an integer node";
+		assert node.getRightExp().getNodeType() == NodeType.INTEGERNODE: "Right node is not an integer node";
+		return (Integer) node.getLeftExp().accept(this) < (Integer) node.getRightExp().accept(this);
+	}
+
+	@Override
+	public Object visitLesserOrEqualsExpNode(LesserOrEqualsExpNode node) {
+		assert node.getLeftExp().getNodeType() == NodeType.INTEGERNODE: "Left node is not an integer node";
+		assert node.getRightExp().getNodeType() == NodeType.INTEGERNODE: "Right node is not an integer node";
+		return (Integer) node.getLeftExp().accept(this) <= (Integer) node.getRightExp().accept(this);
 	}
 }
