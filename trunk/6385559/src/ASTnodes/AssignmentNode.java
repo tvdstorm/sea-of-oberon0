38b4bccb2c91a767fedea54a9a8070034342ff95
benkwint@gmail.com <benkwint@gmail.com@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-01-30 13:26:35 +0000

    added jDepend statistics going to optimize my code

--- trunk/6385559/src/ASTnodes/AssignmentNode.java
+++ trunk/6385559/src/ASTnodes/AssignmentNode.java
@@ -5,26 +5,32 @@ import interpreter.MemoryManager;
 public class AssignmentNode implements StatementNode {
   public AssignmentNode( VarNode var, ExpressionNode expression )
   {
-	this.var = var;
-	this.expression = expression;
+    this.var = var;
+    this.expression = expression;
   }
   
   public void printNode( int depth )
   {
-	for( int i = 0; i < depth; i++ )
-	  System.out.print( " " );
-	if( this.var != null )
-	  this.var.printNode( 0 );
-	System.out.print( " := " );
-	if( this.expression != null )
-	  this.expression.printNode( depth );
-	System.out.println( ";" );
+    for( int i = 0; i < depth; i++ )
+    {
+      System.out.print( " " );
+    }
+    if( this.var != null )
+    {
+      this.var.printNode( 0 );
+    }
+    System.out.print( " := " );
+    if( this.expression != null )
+    {
+      this.expression.printNode( depth );
+    }
+    System.out.println( ";" );
   }
   
   public int eval( String scope )
   {
     if( this.expression != null && this.var != null )
-    {
+    { // only set the value if the expression is an valid expression
       MemoryManager.set( this.var.getVarname( scope ), this.expression.eval( scope ) );      
     }
     return 0;
