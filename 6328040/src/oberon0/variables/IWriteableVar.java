32759e43b1f512ad8778ec3bd972664cecd86357
lennarttange@gmail.com <lennarttange@gmail.com@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-01-31 17:58:51 +0000

    Aaannnnddd, starting all over again

--- 6328040/src/oberon0/variables/IWriteableVar.java
+++
@@ -1,7 +0,0 @@
-package oberon0.variables;
-
-import oberon0.exceptions.IllegalVariableValueException;
-
-public interface IWriteableVar extends IVariable{
-	public void setValue(Object newValue) throws IllegalVariableValueException;
-}
