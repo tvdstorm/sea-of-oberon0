cb483b779a58779c381be8e5e709dd74798ad2f6
benkwint <benkwint@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-01-31 22:07:13 +0000

    metrics en aanpassing aan grammatica

--- trunk/6385559/grammar/parser/oberon0Parser.java
+++ trunk/6385559/grammar/parser/oberon0Parser.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.3 Nov 30, 2010 12:45:30 /Users/benkwint/Documents/uni/softwareConstruction_new/grammar/oberon0.g 2011-01-31 22:06:26
+// $ANTLR 3.3 Nov 30, 2010 12:45:30 /Users/benkwint/Documents/uni/softwareConstruction_new/grammar/oberon0.g 2011-01-31 23:05:09
 
   package parser;
   
