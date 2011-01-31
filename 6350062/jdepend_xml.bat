73d4adebfe5bc3b6f780eb33c41d3d420df59fbd
randy187 <randy187@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-01-31 11:53:10 +0000

    Prefix - mindfuck beter nagebootst in unit tests.

--- 6350062/jdepend_xml.bat
+++ 6350062/jdepend_xml.bat
@@ -1,4 +1,4 @@
 set JDEPEND_HOME=./metrics/jdepend-2.9.1
 set CLASSPATH=%CLASSPATH%;%JDEPEND_HOME%\lib\jdepend-2.9.1.jar
-java jdepend.xmlui.JDepend src/main/java/randy/oberon0/ast src/main/java/randy/oberon0/exception/ src/main/java/randy/oberon0/interpreter src/main/java/randy/oberon0/main src/main/java/randy/oberon0/value
+java jdepend.xmlui.JDepend -components randy.oberon0.ast,randy.oberon0.ast.datastructures,randy.oberon0.ast.enums,randy.oberon0.exception,randy.oberon0.interpreter.antlr,randy.oberon0.interpreter.buildinfunctions,randy.oberon0.interpreter.runtime,randy.oberon0.main,randy.oberon0.value src/main/java/randy/oberon0/ast src/main/java/randy/oberon0/exception/ src/main/java/randy/oberon0/interpreter src/main/java/randy/oberon0/main src/main/java/randy/oberon0/value
 pause
