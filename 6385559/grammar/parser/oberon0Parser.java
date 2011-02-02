a7c519840fd84573f2d45d2ec5546573d4fee397
benkwint@gmail.com <benkwint@gmail.com@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-02-02 21:38:18 +0000

    Expression Termnode deleted, is okidee

--- 6385559/grammar/parser/oberon0Parser.java
+++ 6385559/grammar/parser/oberon0Parser.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.3 Nov 30, 2010 12:45:30 C:\\Documents and Settings\\Ben Kwint\\uva\\softwareConstruction3\\grammar\\oberon0.g 2011-02-02 22:35:22
+// $ANTLR 3.3 Nov 30, 2010 12:45:30 C:\\Documents and Settings\\Ben Kwint\\uva\\softwareConstruction3\\grammar\\oberon0.g 2011-02-02 22:36:54
 
   package parser;
   
