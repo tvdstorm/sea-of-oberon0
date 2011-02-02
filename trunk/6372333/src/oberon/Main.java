4ff3df8cc9971e88e407506b82a63f77760e571c
jwvanbakel <jwvanbakel@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-02-02 20:30:29 +0000

    en wat cleanup

--- trunk/6372333/src/oberon/Main.java
+++ trunk/6372333/src/oberon/Main.java
@@ -7,8 +7,6 @@ import java.io.PushbackReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
-import oberon.data.IdentifierSelector;
-import oberon.data.Selector;
 import oberon.interpret.ModuleInterpreter;
 import oberon.lexer.Lexer;
 import oberon.lexer.LexerException;
@@ -19,53 +17,49 @@ import oberon.parser.ParserException;
 public class Main { 
 	   public static void main(String[] args) { 
 	      if (args.length > 0) { 
-//	         try { 
-	            /* Form our AST */ 
-	            Lexer lexer = null;
-				try {
-					lexer = new PrintLexer (new PushbackReader( 
-					   new FileReader(args[0]), 1024));
-				} catch (FileNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} 
-	            Parser parser = new Parser(lexer); 
-	            Start ast = null;
-				try {
-					ast = parser.parse();
-				} catch (ParserException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (LexerException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} 
-	            
-	            /* Get our Interpreter going. */ 
-	            ModuleInterpreter interpreter = new ModuleInterpreter();
-	            ast.apply(interpreter);	     
-	            
-	            ReadProcedure.Initialize();
-	            WriteCall.Initialize();
-	            WriteLnCall.Initialize();
-	            
-	            Procedure mainProc = interpreter.BuildInterpreterResult();
-	            Queue<Expression> paramList = new LinkedList<Expression>();
-	            	            
-	            try {
-					mainProc.Call(paramList);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-//	         } 
-//	         catch (Exception e) { 
-//	            System.out.println (e) ; 
-//	         } 
-	      } else { 
+			/* Form our AST */ 
+			Lexer lexer = null;
+			try {
+				lexer = new PrintLexer (new PushbackReader( 
+				   new FileReader(args[0]), 1024));
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+			Parser parser = new Parser(lexer); 
+			Start ast = null;
+			try {
+				ast = parser.parse();
+			} catch (ParserException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (LexerException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+			
+			/* Get our Interpreter going. */ 
+			ModuleInterpreter interpreter = new ModuleInterpreter();
+			ast.apply(interpreter);	     
+			
+			ReadProcedure.Initialize();
+			WriteCall.Initialize();
+			WriteLnCall.Initialize();
+			
+			Procedure mainProc = interpreter.BuildInterpreterResult();
+			Queue<Expression> paramList = new LinkedList<Expression>();
+				 
+			try {
+				mainProc.Call(paramList);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	      } 
+	      else { 
 	         System.err.println("usage: java simpleAdder inputFile"); 
 	         System.exit(1); 
 	      } 
