9127141b479c179d97e210e6a7d4152b7d39bedb
lennarttange@gmail.com <lennarttange@gmail.com@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-02-05 15:32:00 +0000

    add comments to context

--- 6328040/src/oberon0/environment/Context.java
+++ 6328040/src/oberon0/environment/Context.java
@@ -4,6 +4,10 @@ import java.util.HashMap;
 
 import oberon0.ast.routines.ICallable;
 
+/*
+ * This is where all known variables, procedures and types can be stored
+ * Also known as Environment 
+ */
 public class Context {	
 	private String _name;
 	private HashMap<String, Reference> _variables;
@@ -11,7 +15,9 @@ public class Context {
 	private HashMap<String, IValue> _types;
 	private Context _parent;
 	
-	//a better looking 'macro' when there is no parent context
+	/*
+	 * when there is no parent, use this 'null'-value to increase readability
+	 */
 	public final static Context noParent = null;
 	
 	public Context (String name, Context parent){
@@ -22,8 +28,11 @@ public class Context {
 		_parent = parent;
 	}
 	
+	/*
+	 * Get a reference from the variable scope with the given name
+	 */
 	public Reference getReference(String name){
-		if (!contains(name)){
+		if (!containsVariable(name)){
 			if (_parent != noParent){
 				return _parent.getReference(name);
 			}
@@ -32,30 +41,68 @@ public class Context {
 		return _variables.get(name);
 	}
 	
+	/*
+	 * returns true if name exists in current variablescope (_variables)
+	 * otherwise returns false
+	 */
+	private boolean containsVariable(String name){
+		return _variables.containsKey(name);
+	}
+	
+	/*
+	 * Get a procedure known in the current scope with the given name
+	 */
 	public ICallable getProcedure(String name){
 		return _procedures.get(name);
 	}
 	
-	public void declareVariable(String name, IValue value) {
-		_variables.put(name, new Reference (value));
+	/*
+	 * Get the type with the given name from the current scope 
+	 */
+	public IValue getType(String name){
+		return _types.get(name);
 	}
 	
+	/*
+	 * declare a new type in the current scope by a name and a value
+	 */
+	public void declareType(String name, IValue type){
+		_types.put(name, type);
+	}
+	
+	/*
+	 * declares a name and value in the current variablescope
+	 * (makes a new reference from the given value 
+	 */
+	public void declareVariable(String name, IValue value) {
+		_variables.put(name, new Reference(value));
+	}
+	
+	/*
+	 * declares a name and reference in the current variablescope
+	 */
 	public void declareReferenceVariable(String name, Reference reference){
 		_variables.put(name, reference);
 	}
 	
+	/*
+	 * declares a constant reference in the current variablescope
+	 */
 	public void declareConstant(String name, IValue value) {
-		_variables.put(name, new ConstantReference (value));
+		_variables.put(name, new ConstantReference(value));
 	}
 	
+	/*
+	 * declares a procedure in the current procedurescope
+	 */
 	public void declareProcedure(String name, ICallable procedure){
 		_procedures.put(name, procedure);
 	}
 	
-	public boolean contains(String name){
-		return _variables.containsKey(name);
-	}
-	
+	/*
+	 * Prints the current context
+	 * can be used for debugging and testing
+	 */
 	public void print(){
 		System.out.print("Known variables in current scope " + _name +":\n");
 		for(String key: _variables.keySet()){
@@ -67,14 +114,9 @@ public class Context {
 		}
 	}
 	
-	public IValue getType(String name){
-		return _types.get(name);
-	}
-	
-	public void declareType(String name, IValue type){
-		_types.put(name, type);
-	}
-	
+	/*
+	 * returns the parentscope
+	 */
 	public Context getParent(){
 		return _parent;
 	}
