79126651ad1810f61b0a65356612706ff067bd04
pbrandwijk <pbrandwijk@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-02-06 18:26:00 +0000

    Added base class for statements

---
+++ trunk/6385028/oberon-0/src/main/java/org/elcid/oberon0/ast/StatementNode.java
@@ -0,0 +1,10 @@
+package org.elcid.oberon0.ast;
+
+/**
+ * Base class for statements in the Oberon-0 language.
+ *
+ * @author Pieter Brandwijk
+ */
+public abstract class StatementNode extends Node {
+
+}
