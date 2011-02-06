705b5b7843e47aba68b3ff8e0dc3133fc44577f5
randy187@gmail.com <randy187@gmail.com@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-02-06 11:46:49 +0000

    JavaNCSS en JDepend aangepast i.v.m. recente wijzigingen.

--- 6350062/javancss_txt.bat
+++ 6350062/javancss_txt.bat
@@ -1,1 +1,1 @@
-metrics\javancss-32.53\bin\javancss.bat -all -recursive src/main/java/randy/oberon0/ast src/main/java/randy/oberon0/exception src/main/java/randy/oberon0/interpreter src/main/java/randy/oberon0/main src/main/java/randy/oberon0/value > javancss.txt
+metrics\javancss-32.53\bin\javancss.bat -all -recursive src/main/java/randy/oberon0/ast src/main/java/randy/oberon0/exception src/main/java/randy/oberon0/interpreter src/main/java/randy/oberon0/main src/main/java/randy/oberon0/value src/main/java/randy/library > javancss.txt
