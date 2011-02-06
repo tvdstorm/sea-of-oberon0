7566523e8afae8f2c7025978a1985e6b9910ca57
israelposner <israelposner@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-02-06 11:22:10 +0000

    remove bin files

--- trunk/6354009/Oberon0/src/oberon0/interpreter/statement/Statement.java
+++
@@ -1,8 +0,0 @@
-package oberon0.interpreter.statement;
-
-import oberon0.interpreter.exceptions.ExecutionException;
-import oberon0.interpreter.module.Module;
-
-public interface Statement {
-	public void execute(Module module) throws ExecutionException;
-}
