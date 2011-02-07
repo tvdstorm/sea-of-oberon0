f1f0e56618c414db44d58df98f411b751850bead
pbrandwijk <pbrandwijk@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-02-07 17:42:54 +0000

    Removed node type

--- 6385028/oberon-0/src/main/java/org/elcid/oberon0/ast/Node.java
+++ 6385028/oberon-0/src/main/java/org/elcid/oberon0/ast/Node.java
@@ -1,10 +1,7 @@
 package org.elcid.oberon0.ast;
 
-import org.elcid.oberon0.ast.util.NodeType;
 import org.elcid.oberon0.ast.env.Environment;
 import org.elcid.oberon0.ast.visitor.BaseVisitor;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Base class for all nodes in the AST.
