9e531e4ceee207d1614cd36212fcdd40437e51cd
pbrandwijk <pbrandwijk@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-02-08 12:45:48 +0000

    Changed value type to Value

--- trunk/6385028/oberon-0/src/main/java/org/elcid/oberon0/ast/env/Environment.java
+++ trunk/6385028/oberon-0/src/main/java/org/elcid/oberon0/ast/env/Environment.java
@@ -2,6 +2,7 @@ package org.elcid.oberon0.ast.env;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.elcid.oberon0.ast.values.Value;
 import org.elcid.oberon0.exceptions.UnboundVariableException;
 
 /**
@@ -12,10 +13,10 @@ import org.elcid.oberon0.exceptions.UnboundVariableException;
  */
 public class Environment {
 
-	private Map<String, Integer> map;
+	private Map<String, Value> map;
 
 	public Environment() {
-		map = new HashMap<String, Integer>();
+		map = new HashMap<String, Value>();
 	}
 
 	/**
@@ -26,8 +27,8 @@ public class Environment {
 	 * @param	variableName
 	 * @return	the integer that is bound to the variable
 	 */
-	public Integer getValue(String variableName) {
-		Integer value = map.get(variableName);
+	public Value getValue(String variableName) {
+		Value value = map.get(variableName);
 		if (value == null)
 			throw new UnboundVariableException("Variable " + variableName + " is not bound to an integer");
 		return value;
@@ -40,10 +41,10 @@ public class Environment {
 	 * @param variableName
 	 * @param value
 	 */
-	public void put(String variableName, Integer value) {
+	public void put(String variableName, Value value) {
 		assert (variableName != null) : "Variable name is null";
 		assert (!variableName.equals("")) : "Variable name is empty string";
-		assert (value != null) : "Integer value is null";
+		assert (value != null) : "Value is null";
 		map.put(variableName, value);
 	}
 
