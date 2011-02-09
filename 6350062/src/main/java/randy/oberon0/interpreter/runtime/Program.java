23e78d6aff3a46a84d398703f2e46100da179d1c
randy187@gmail.com <randy187@gmail.com@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-02-09 14:22:58 +0000

    Package refactoring.

--- 6350062/src/main/java/randy/oberon0/interpreter/runtime/Program.java
+++ 6350062/src/main/java/randy/oberon0/interpreter/runtime/Program.java
@@ -6,7 +6,7 @@ import randy.oberon0.interpreter.buildinfunctions.*;
 import randy.oberon0.interpreter.runtime.environment.*;
 import randy.oberon0.interpreter.typecheck.environment.*;
 import randy.oberon0.value.Type;
-import randy.oberon0.ast.*;
+import randy.oberon0.ast.module.Module;
 import randy.oberon0.exception.RuntimeException;
 import randy.oberon0.exception.Exception;
 
