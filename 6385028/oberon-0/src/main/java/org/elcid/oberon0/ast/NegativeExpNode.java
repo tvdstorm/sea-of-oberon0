6dbcdd9ae0ca7f883d0682dac9d6789bd5ec9f2e
pbrandwijk <pbrandwijk@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-02-10 19:11:23 +0000

    Removed BaseVisitor

--- 6385028/oberon-0/src/main/java/org/elcid/oberon0/ast/NegativeExpNode.java
+++ 6385028/oberon-0/src/main/java/org/elcid/oberon0/ast/NegativeExpNode.java
@@ -1,7 +1,7 @@
 package org.elcid.oberon0.ast;
 
 import org.elcid.oberon0.ast.env.Environment;
-import org.elcid.oberon0.ast.visitor.BaseVisitor;
+import org.elcid.oberon0.ast.visitor.ExpressionVisitor;
 
 /**
  * Represents the not ("~") expression in the Oberon-0 language.
@@ -23,7 +23,7 @@ public class NegativeExpNode extends ExpressionNode {
 	}
 
 	@Override
-	public Object eval(BaseVisitor visitor, Environment localEnv) {
+	public Object eval(ExpressionVisitor visitor, Environment localEnv) {
 		return visitor.eval(this, localEnv);
 	}
 
