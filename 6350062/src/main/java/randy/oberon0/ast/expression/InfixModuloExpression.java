b4f409c8394abef0b6da4056a26057812479cfa2
randy187@gmail.com <randy187@gmail.com@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-02-10 11:25:05 +0000

    Modulo vergeten, nu geimplementeerd.

---
+++ 6350062/src/main/java/randy/oberon0/ast/expression/InfixModuloExpression.java
@@ -0,0 +1,52 @@
+package randy.oberon0.ast.expression;
+
+import randy.oberon0.exception.OperatorTypeUndefinedException;
+import randy.oberon0.exception.RuntimeException;
+import randy.oberon0.interpreter.runtime.environment.*;
+import randy.oberon0.interpreter.typecheck.environment.*;
+import randy.oberon0.value.Value;
+import randy.oberon0.value.Integer;
+
+public class InfixModuloExpression extends InfixExpression
+{
+	public InfixModuloExpression(Expression _leftHandExpression, Expression _rightHandExpression)
+	{
+		super(_leftHandExpression, _rightHandExpression);
+	}
+	@Override
+	public IBindableValue evaluate(RuntimeEnvironment environment) throws RuntimeException
+	{
+		assert(environment != null);
+		// Evaluate the left and right hand side expressions
+		final Value valRh = rightHandExpression.evaluate(environment).getValue();
+		final Value valLh = leftHandExpression.evaluate(environment).getValue();
+		// Check if we support the operator
+		if (valLh instanceof Integer && valRh instanceof Integer)
+		{
+			return new ByValue(new Integer(valLh.castToInteger().getIntValue() % valRh.castToInteger().getIntValue()));
+		}
+		else
+		{
+			// No, throw an exception
+			throw new OperatorTypeUndefinedException("*", valLh.getType().toString(), valRh.getType().toString());
+		}
+	}
+	@Override
+	public ITypeCheckBindableValue typeCheck(TypeCheckEnvironment environment) throws RuntimeException
+	{
+		assert(environment != null);
+		// Evaluate the left and right hand side expressions
+		final ITypeCheckType valRh = rightHandExpression.typeCheck(environment).getValue();
+		final ITypeCheckType valLh = leftHandExpression.typeCheck(environment).getValue();
+		// Check if we support the operator
+		if (valLh.equals(TypeCheckType.INTEGER) && valRh.equals(TypeCheckType.INTEGER))
+		{
+			return new TypeCheckByValue(TypeCheckType.INTEGER);
+		}
+		else
+		{
+			// No, throw an exception
+			throw new OperatorTypeUndefinedException("MOD", valLh.toString(), valRh.toString());
+		}
+	}
+}
