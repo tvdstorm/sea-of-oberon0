0052a7f03901dd0289f57eec322c696d6c01dd83
bartvaneijkelenburg@gmail.com <bartvaneijkelenburg@gmail.com@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-02-11 15:28:27 +0000

    deleted some replaced files

--- 6355846/src/main/java/nl/bve/uva/oberon/ast/IdentChangerNode.java
+++
@@ -1,9 +0,0 @@
-package nl.bve.uva.oberon.ast;
-
-import java.util.List;
-
-public class IdentChangerNode extends IdentSelectorNode {
-	public IdentChangerNode(String ident, List<IInterpretableNode> selectors ) {
-		super(ident, selectors);
-	}
-}
