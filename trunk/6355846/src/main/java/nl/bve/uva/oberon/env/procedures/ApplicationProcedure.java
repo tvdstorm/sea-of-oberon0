1ffb87f898fc720f3c8bbe850b38b9bae203c3a2
bartvaneijkelenburg@gmail.com <bartvaneijkelenburg@gmail.com@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-02-12 21:20:37 +0000

    generalized procedurecalls

--- trunk/6355846/src/main/java/nl/bve/uva/oberon/env/procedures/ApplicationProcedure.java
+++ trunk/6355846/src/main/java/nl/bve/uva/oberon/env/procedures/ApplicationProcedure.java
@@ -1,14 +1,12 @@
 package nl.bve.uva.oberon.env.procedures;
 
 import java.util.List;
-import java.util.ListIterator;
 
 import nl.bve.uva.oberon.ast.IInterpretableNode;
 import nl.bve.uva.oberon.ast.TypedParameterList;
 import nl.bve.uva.oberon.env.Environment;
-import nl.bve.uva.oberon.env.types.Type;
 
-public class ApplicationProcedure implements Procedure {
+public class ApplicationProcedure extends Procedure {
 	private String name;
 	private List<TypedParameterList> formalParametersList;
 	private IInterpretableNode body;	
@@ -20,38 +18,17 @@ public class ApplicationProcedure implements Procedure {
 	}
 	
 	@Override
-	public void callProcedure(Environment env, List<IInterpretableNode> actualParameters) {
-		if (formalParametersList == null && actualParameters != null) {
-			throw new RuntimeException("Too many actual values provided for procedure " +name);
-		}
-		if (formalParametersList != null && actualParameters == null) {
-			throw new RuntimeException("No actual values given for procedure " +name+ " (parameters needed: " +formalParametersList.size());
-		}
-		
-		Environment subEnv = env.getNewSubSpace();
-		
-		if (formalParametersList != null && actualParameters != null) {
-			ListIterator<IInterpretableNode> actuals = actualParameters.listIterator();
-			
-			for (TypedParameterList fpList : formalParametersList) {
-				List<String> identList = fpList.getParametersList();
-				
-				for (String ident : identList) {
-					if (!actuals.hasNext()) {
-						throw new RuntimeException("Not enough actual values given for procedure " +name+ " (parameters needed: " +formalParametersList.size());
-					}
-					
-					Type value = (Type)actuals.next().interpret(subEnv);
-					value = fpList.processValue(ident, value, subEnv);
-					subEnv.addVariable(ident, value);
-				}
-			}
-			
-			if (actuals.hasNext()) {
-				throw new RuntimeException("Too many actual values given for procedure " +name+ " (parameters needed: " +formalParametersList.size());
-			}
-		}
-		
-		body.interpret(subEnv);
+	public List<TypedParameterList> getFormalParameters() {
+		return formalParametersList;
+	}
+	
+	@Override
+	public String getName() {
+		return name;
+	}
+	
+	@Override
+	public void execute(Environment env) {
+		body.interpret(env);
 	}
 }
