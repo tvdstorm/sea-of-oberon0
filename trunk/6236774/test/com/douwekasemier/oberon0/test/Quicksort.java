6b92f955f7a8ad509915312a950960339c902cd5
dkasemier@gmail.com <dkasemier@gmail.com@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-02-15 12:01:47 +0000

    VERSIE 1 AF

--- trunk/6236774/test/com/douwekasemier/oberon0/test/Quicksort.java
+++ trunk/6236774/test/com/douwekasemier/oberon0/test/Quicksort.java
@@ -13,9 +13,9 @@ public class Quicksort {
         // Laad een bestand oberon0 bestand     
         Interpreter interpreter = new Interpreter();
         File file = new File("testscripts/quicksort.oberon0");
-        String input = "5\n1\n4\n2\n3\n";
-        for( int i = 0; i < 5; i++ ) {
-            //input += ((int)(Math.random() * 98) + 1) + "\n";
+        String input = "";
+        for( int i = 0; i < 10; i++ ) {
+            input += ((int)(Math.random() * 98) + 1) + "\n";
         }
         interpreter.interpret(file, input);
     }
