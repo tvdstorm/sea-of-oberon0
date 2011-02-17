cf5cfe1adc94d24bfcc0563cc05a38fce174fb0a
randy187@gmail.com <randy187@gmail.com@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-02-17 15:37:08 +0000

    Merged r666-667.

---
+++ trunk/6350062_assignment_1/src/main/java/randy/oberon0/interpreter/runtime/datastructures/InvokableFunctionAndEnvironment.java
@@ -0,0 +1,26 @@
+package randy.oberon0.interpreter.runtime.datastructures;
+
+import randy.oberon0.ast.IInvokableFunction;
+import randy.oberon0.interpreter.runtime.RuntimeEnvironment;
+
+public class InvokableFunctionAndEnvironment
+{
+	private final IInvokableFunction function;
+	private final RuntimeEnvironment environment;
+	
+	public InvokableFunctionAndEnvironment(IInvokableFunction _function, RuntimeEnvironment _environment)
+	{
+		assert(_function != null);
+		assert(_environment != null);
+		function = _function;
+		environment = _environment;
+	}
+	public IInvokableFunction getFunction()
+	{
+		return function;
+	}
+	public RuntimeEnvironment getEnvironment()
+	{
+		return environment;
+	}
+}
