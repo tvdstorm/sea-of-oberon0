da33523955254f6801d9060cb3dc7aa4c7972248
jandemooij <jandemooij@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-02-19 14:10:05 +0000

    -

--- 6242944/interpreter/tools/run_javancss.sh
+++ 6242944/interpreter/tools/run_javancss.sh
@@ -1,2 +1,2 @@
-tools/javancss-32.53/bin/javancss -xml -recursive -all -out data/javancss.xml src/jdm/oberon0/ast src/jdm/oberon0/builtins src/jdm/oberon0/exceptions src/jdm/oberon0/interpreter src/jdm/oberon0/main src/jdm/oberon0/types src/jdm/oberon0/values
+tools/javancss-32.53/bin/javancss -xml -recursive -all -out data/javancss.xml src/jdm/oberon0/ast src/jdm/oberon0/builtins src/jdm/oberon0/exceptions src/jdm/oberon0/interpreter src/jdm/oberon0/main src/jdm/oberon0/types src/jdm/oberon0/values src/jdm/oberon0/typechecker
 
--- 6242944/interpreter/tools/run_jdepend.sh
+++ 6242944/interpreter/tools/run_jdepend.sh
@@ -1,2 +1,2 @@
-java -classpath tools/jdepend-2.9.1/lib/jdepend-2.9.1.jar jdepend.xmlui.JDepend -file data/jdepend.xml bin/jdm/oberon0/ast bin/jdm/oberon0/builtins bin/jdm/oberon0/exceptions bin/jdm/oberon0/interpreter bin/jdm/oberon0/main bin/jdm/oberon0/types bin/jdm/oberon0/values
+java -classpath tools/jdepend-2.9.1/lib/jdepend-2.9.1.jar jdepend.xmlui.JDepend -file data/jdepend.xml bin/jdm/oberon0/ast bin/jdm/oberon0/builtins bin/jdm/oberon0/exceptions bin/jdm/oberon0/interpreter bin/jdm/oberon0/main bin/jdm/oberon0/types bin/jdm/oberon0/values bin/jdm/oberon0/typechecker
 
