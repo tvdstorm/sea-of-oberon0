6e3470d913c45da189171e54c4451d4142845db3
pbrandwijk <pbrandwijk@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-02-19 16:54:16 +0000

    Added tests for if statement

--- 6385028/oberon-0/src/test/java/org/elcid/oberon0/ast/visitor/StatementVisitorTest.java
+++ 6385028/oberon-0/src/test/java/org/elcid/oberon0/ast/visitor/StatementVisitorTest.java
@@ -2,9 +2,12 @@ package org.elcid.oberon0.ast.visitor;
 
 import org.elcid.oberon0.visitors.StatementVisitor;
 import org.elcid.oberon0.ast.AssignmentNode;
+import org.elcid.oberon0.ast.EqualsExpNode;
 import org.elcid.oberon0.ast.ExpressionNode;
 import org.elcid.oberon0.ast.IdentSelectorNode;
+import org.elcid.oberon0.ast.IfStmNode;
 import org.elcid.oberon0.ast.IntExpNode;
+import org.elcid.oberon0.ast.StatementSequenceNode;
 import org.elcid.oberon0.values.Int;
 import org.elcid.oberon0.env.Environment;
 import org.junit.After;
@@ -64,6 +67,25 @@ public class StatementVisitorTest {
 
 	@Test
 	public void testRun_IfStmNode_Environment() {
+
+		// Test with true expression
+		env.declareValue(A_VARIABLE_NAME, new Int(1));
+		StatementSequenceNode ssTrue = new StatementSequenceNode();
+		ssTrue.addStatement(new AssignmentNode(new IdentSelectorNode(A_VARIABLE_NAME), new IntExpNode(5)));
+		IfStmNode ifNodeTrue = new IfStmNode(new EqualsExpNode(new IntExpNode(2), new IntExpNode(2)), ssTrue);
+		ifNodeTrue.run(instance, env);
+		int resultTrue = ((Int)env.getValue(A_VARIABLE_NAME)).getValue();
+		int expResultTrue = 5;
+		assertEquals(expResultTrue, resultTrue);
+
+		// Test with false expression
+		StatementSequenceNode ssFalse = new StatementSequenceNode();
+		ssFalse.addStatement(new AssignmentNode(new IdentSelectorNode(A_VARIABLE_NAME), new IntExpNode(6)));
+		IfStmNode ifNodeFalse = new IfStmNode(new EqualsExpNode(new IntExpNode(2), new IntExpNode(3)), ssFalse);
+		ifNodeFalse.run(instance, env);
+		int resultFalse = ((Int)env.getValue(A_VARIABLE_NAME)).getValue();
+		int expResultFalse = 5;
+		assertEquals(expResultFalse, resultFalse);
 	}
 
 	@Test
