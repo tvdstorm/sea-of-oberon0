cc71563957d96d57b4f85b2712024ba0c3204a13
bartvaneijkelenburg@gmail.com <bartvaneijkelenburg@gmail.com@085e46ef-660c-bccc-452f-9e56fe5268ae>
2011-02-19 22:32:05 +0000

    parser/lexer update

--- 6355846/src/main/java/nl/bve/uva/oberon/parser/Oberon0Lexer.java
+++ 6355846/src/main/java/nl/bve/uva/oberon/parser/Oberon0Lexer.java
@@ -3,10 +3,14 @@
 package nl.bve.uva.oberon.parser;
 
 
-import org.antlr.runtime.*;
-import java.util.Stack;
-import java.util.List;
-import java.util.ArrayList;
+import org.antlr.runtime.BaseRecognizer;
+import org.antlr.runtime.CharStream;
+import org.antlr.runtime.DFA;
+import org.antlr.runtime.EarlyExitException;
+import org.antlr.runtime.Lexer;
+import org.antlr.runtime.MismatchedSetException;
+import org.antlr.runtime.RecognitionException;
+import org.antlr.runtime.RecognizerSharedState;
 
 public class Oberon0Lexer extends Lexer {
     public static final int EOF=-1;
